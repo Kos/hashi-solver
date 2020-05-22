272a8845b74f59b8b736d3a06ca8bbf5d058f23a
Tomasz Wesołowski <kosashi@gmail.com>
2020-05-22 23:10:11 +0200

    Presentation layer - initial

---
+++ src/index.tsx
@@ -0,0 +1,117 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import * as easyStarters from "../puzzles/conceptis-easy-starters.json";
+import { Puzzle, Solution } from "./types";
+import { solve } from "./solver";
+
+function MyElem() {
+  const puzzle = Puzzle.fromObject(easyStarters.puzzles[0]);
+  const solution = solve(puzzle);
+  console.log(puzzle);
+  console.log(solution);
+
+  return (
+    <div>
+      <MySvg puzzle={puzzle} solution={solution} />
+    </div>
+  );
+}
+
+function MySvg({ puzzle, solution }: { puzzle: Puzzle; solution: Solution }) {
+  const scale = 60;
+  const offset = scale / 2;
+  const width = puzzle.width * scale;
+  const height = puzzle.height * scale;
+  const place = (x) => x * scale + offset;
+  return (
+    <svg width={width} height={height} xmlns="http://www.w3.org/2000/svg">
+      {solution.bridges.map((bridge, index) => (
+        <Bridge
+          key={index}
+          x1={place(puzzle.islands[bridge.from].x)}
+          y1={place(puzzle.islands[bridge.from].y)}
+          x2={place(puzzle.islands[bridge.to].x)}
+          y2={place(puzzle.islands[bridge.to].y)}
+          double={bridge.value == 2}
+        />
+      ))}
+      {puzzle.islands.map((island, index) => (
+        <Circle
+          key={index}
+          cx={place(island.x)}
+          cy={place(island.y)}
+          text={island.value + ""}
+        />
+      ))}
+    </svg>
+  );
+}
+
+function Bridge({ x1, x2, y1, y2, double = false }) {
+  const spread = 4;
+  const offsetIndices = double ? [1, 2] : [0];
+  const offsets =
+    x1 == x2
+      ? [
+          { x: 0, y: 0 },
+          { x: -spread, y: 0 },
+          { x: spread, y: 0 },
+        ]
+      : [
+          { x: 0, y: 0 },
+          { x: 0, y: -spread },
+          { x: 0, y: spread },
+        ];
+  return (
+    <g>
+      {offsetIndices.map((i) => (
+        <line
+          key={i}
+          x1={x1 + offsets[i].x}
+          y1={y1 + offsets[i].y}
+          x2={x2 + offsets[i].x}
+          y2={y2 + offsets[i].y}
+          strokeWidth="1.5"
+          stroke="#000"
+          fill="none"
+        />
+      ))}
+    </g>
+  );
+}
+
+function Circle({ cx = 100, cy = 100, text = "1" }) {
+  return (
+    <g>
+      <ellipse
+        ry="25"
+        rx="25"
+        cy={cy}
+        cx={cx}
+        strokeWidth="1.5"
+        stroke="#000"
+        fill="#fff"
+      />
+      <text
+        xmlSpace="preserve"
+        textAnchor="middle"
+        dominantBaseline="middle"
+        fontFamily="Helvetica, Arial, sans-serif"
+        fontSize="24"
+        id="svg_2"
+        x={cx}
+        y={cy}
+        strokeWidth="0"
+        stroke="#000"
+        fill="#000000"
+      >
+        {text}
+      </text>
+    </g>
+  );
+}
+
+const div = document.createElement("div");
+document.body.appendChild(div);
+ReactDOM.render(<MyElem />, div);
